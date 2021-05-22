91cf49aa4c8762dba32d58cce435009c44f1870a
Chi Trung Nguyen <n.chitrung@castsoftware.com>
2021-05-22 18:59:06 +0200

    WRITE READ 4 processes

--- ViewModels/Customer.cs
+++ ViewModels/Customer.cs
@@ -13,13 +13,17 @@ namespace ViewModels
         public ICommand ProduceCommand { get; }
         public ICommand ProduceCommand2 { get; }
         public ICommand ConsumeCommand { get; }
+        public ICommand ConsumeCommand2 { get; }
         public ICommand QueueCommand { get; }
         public ICommand StopCommand { get; }
         public ICommand StopProduce { get; }
+        public ICommand StopProduce2 { get; }
         public ICommand StopConsumer1 { get; }
+        public ICommand StopConsumer2 { get; }
         public IShell ProducerShell2 { get; set; }
         public IShell ProducerShell { get; set; }
         public IShell Consummer { get; set; }
+        public IShell Consummer2 { get; set; }
         public IShell QueueShell { get; set; }
         
 
@@ -27,6 +31,7 @@ namespace ViewModels
         {
            // MyCommand = new CustomerCommand(ExcuteMethod, CanExecuteMethod);           
             Consummer = new Shell() { StatusExecutable = true };
+            Consummer2 = new Shell() { StatusExecutable = true };
             QueueShell = new Shell() { StatusExecutable = true };
             ProducerShell = new Shell() { StatusExecutable =true};
             ProducerShell2 = new Shell() { StatusExecutable =true};
@@ -35,10 +40,14 @@ namespace ViewModels
             ProduceCommand2 = new ProduceCommand(ProducerShell2, QueueShell);
             ProduceCommand = new ProduceCommand(ProducerShell, QueueShell);
             ConsumeCommand = new ConsumeCommand(Consummer,QueueShell);
+            ConsumeCommand2 = new ConsumeCommand(Consummer2,QueueShell);
             QueueCommand = new QueueCommandG<string>(QueueShell);
             StopCommand = new StopCommand(QueueShell);
+
             StopProduce = new StopTask(ProducerShell);            
+            StopProduce2 = new StopTask(ProducerShell2);            
             StopConsumer1 = new StopTask(Consummer);            
+            StopConsumer2 = new StopTask(Consummer2);            
         }
 
 
